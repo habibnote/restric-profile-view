8d30f6ec27dffa49ebe450fca435664240198a78
habibnote <habibblog97@gmail.com>
2023-11-14 14:04:28 +0600

    all assets file created and load form css file

--- inc/Shortcode.php
+++ inc/Shortcode.php
@@ -8,8 +8,17 @@ class Shortcode {
      * class constructor
      */
     function __construct() {
-        
+        add_action( 'wp_enqueue_scripts', [$this, 'rpv_load_assets'] );
         add_shortcode( 'rpv_shortcode', [$this, 'rpv_main_shortcode'] );
+    }
+
+    /**
+     * Load shortcode assets
+     */
+    function rpv_load_assets() {
+        if( ! is_admin() ) {
+            wp_enqueue_style( 'shortcode-form', RPV_ASSET . '/front/css/form.css', array(), time() );
+        } ;
     }
 
     /**
