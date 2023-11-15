7f3c7c60b5b5d7b29d07705be6860d5809511c29
habibnote <habibblog97@gmail.com>
2023-11-15 10:44:24 +0600

    intial loop done

--- parts/profiles.php
+++ parts/profiles.php
@@ -1,25 +1,42 @@
 
 <div class="rpv-profiles-container">
-    <div class="rpv-single-profile">
-        <div class="rpv-image-area">
-            <div class="rvp-logo">
-                <img src="https://corporate-africa.com/wp-content/uploads/2023/08/abulink.png" alt="">
-            </div>
-            <div class="rpv-profile-pic">
-                <img src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg" alt="">
-            </div>
-        </div>
-        <div class="rpv-content-area">
 
-        </div>
-        <div class="rpv-profile-desc">
-            
-        </div>
-        <div class="rpv-start-chat">
-            <a href="https://corporate-africa.com/send-message/"> <?php _e( 'Start chat', 'restric-p-v' ) ?> </a>
-        </div>
-    </div>
-    <div class="rpv-single-profile">
+    <?php 
+        $args = array(
+            'post_type' => 'profiles',
+            'posts_per_page' => 1,
+            'order' => 'DESC',
+            'orderby' => 'date',
+        );
+        
+        $query = new WP_Query($args);
+        while( $query->have_posts() ) {
+            $query->the_post();
+            ?> 
+                <div class="rpv-single-profile">
+                    <div class="rpv-image-area">
+                        <div class="rvp-logo">
+                            <img src="https://corporate-africa.com/wp-content/uploads/2023/08/abulink.png" alt="">
+                        </div>
+                        <div class="rpv-profile-pic">
+                            <img src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg" alt="">
+                        </div>
+                    </div>
+                    <div class="rpv-content-area">
+
+                    </div>
+                    <div class="rpv-profile-desc">
+                        
+                    </div>
+                    <div class="rpv-start-chat">
+                        <a href="https://corporate-africa.com/send-message/"> <?php _e( 'Start chat', 'restric-p-v' ) ?> </a>
+                    </div>
+                </div>
+            <?php 
+        }
+    ?>
+
+    <!-- <div class="rpv-single-profile">
         <div class="rpv-image-area">
             <div class="rvp-logo">
                 <img src="https://corporate-africa.com/wp-content/uploads/2023/08/abulink.png" alt="">
@@ -37,5 +54,5 @@
         <div class="rpv-start-chat">
         <a href="https://corporate-africa.com/send-message/"> <?php _e( 'Start chat', 'restric-p-v' ) ?> </a>
         </div>
-    </div>
+    </div> -->
 </div>
